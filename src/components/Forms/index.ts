eb246f7d13301985eac273c80932b39eae95fca2
Elena <msle93@gmail.com>
2021-05-24 15:49:14 +0400

    Updates

--- src/components/Forms/index.ts
+++
@@ -1,5 +0,0 @@
-import TimePeriodForecastForm from './TimePeriodForecastForm';
-import DailyForecastInPastForm from './DailyForecastInPastForm';
-import Form from './Form';
-
-export { TimePeriodForecastForm, DailyForecastInPastForm, Form };
